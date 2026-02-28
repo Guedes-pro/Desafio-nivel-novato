3424f32fe3421ecc7026b92cfc695763016851e5
Guedes-pro <sperex2012@gmail.com>
2026-02-28 23:23:05 +0000

    tema 3 - nível novato

---
+++ Tema 3 Nível novato/super_trunfo.c
@@ -0,0 +1,204 @@
+#include <stdio.h>
+ 
+int main(){
+
+     // Declaração de variáveis utilizando arrays, nas variáveis de int e float, para armazenar dados de múltiplas cartas.
+     // Será usado o índice [0] para Carta 1 e o índice [1] para Carta 2.
+
+     char estado1, estado2;
+     char codigo1[5], codigo2[5];
+     char cidade1[30], cidade2[30];
+     int populacao[2];
+     int pontos_turisticos[2];
+     float area[2];
+     float pib[2];
+     float pib_per[2], populcao_den[2];
+     float super_poder[2];
+     
+
+
+
+     //------ PRIMEIRA CARTA ------
+
+     printf("Informe os dados da primeira carta \n\n");
+     // Para adicionar novos atributos à Carta 1, adcione conforme o exemplo:
+     // printf("*ATRITBUTO A SER UTILIZADO: *")
+     // scanf("ESPECIFICADOR DE FORMATO", VARIÁVEL *se for o caso adcione o índicie[0]*)
+     // Use o *getchar()* após o scanf  do codigo para "limpar" o caractere que fica sobrando. Se não usar, o fgets lê o vazio.
+     // Use o fgets para inserção das cidades, pois algumas tem nomes compostos.
+
+     printf("Estado: ");
+     scanf("%c", &estado1);
+     printf("Código: ");
+     scanf("%s", codigo1);// Por ser string não é necessário o "&".
+     getchar(); 
+     printf("Nome da Cidade: ");
+     fgets(cidade1, 30, stdin);
+     cidade1[strcspn(cidade1, "\n")]='\0';//Use o *='\0'*, para não deixar o espaço em branco apos *Popuação*.
+     printf("População: ");
+     scanf("%d", &populacao[0]);
+     printf("Área (em km²): ");
+     scanf("%f", &area[0]);
+     printf("PIB (em bihões): ");
+     scanf("%f", &pib[0]);
+     printf("Número de Pontos Turísticos: ");
+     scanf("%d", &pontos_turisticos[0]);
+
+     // **Cálculo do PIB per Capita, Densidade Populacional e do Super Poder**
+     // Feito a Conversão Explicita na variável população
+     
+
+     pib_per[0] = (pib[0] *  pow(10, 9)) / (float) populacao[0]; 
+     populcao_den[0] = (float) populacao[0] / area[0];
+     super_poder[0] = (float) populacao[0] + (float) pontos_turisticos[0] + pib[0] +
+                        area[0] + pib_per[0] + (1 / populcao_den[0]); 
+
+     
+
+     
+     
+     // ---LEMBRAR---
+     //*TODOS* os atributos adicionados terão de aparecer na apresentação da carta.
+     /* Exemplo:
+      printf("\nCarta 1\n");
+      printf("*ATRIBUTO ADICIONADO*: ESPECIFICADOR DE FORMATO\n", VARIÁVEL);*/
+
+     printf("\nCarta 1\n");
+     printf("Estado: %c\n", estado1);
+     printf("Código: %s\n", codigo1);
+     printf("Nome da Cidade: %s\n", cidade1);
+     printf("População: %d\n", populacao[0]);
+     printf("Área: %.2f km²\n", area[0]);
+     printf("PIB: %.2f bilhões de reais\n", pib[0]);
+     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos[0]);
+     printf("Densidade Populacional: %.2f hab/km²\n", populcao_den[0]);
+     printf("PIB per Capita: %.2f reais\n", pib_per[0]);
+     printf("Super Poder: %.2f\n\n", super_poder[0]);
+     
+     
+     //------ SEGUNDA CARTA ------
+
+     printf("Informe os dados da segunda carta: \n\n");
+     // Para adicionar novos atributos à Carta 2, adcione conforme o exemplo:
+     // printf("*ATRITBUTO A SER UTILIZADO: *")
+     // scanf("ESPECIFICADOR DE FORMATO", VARIÁVEL *se for o caso adcione o índicie[1]*)
+     // Use o *getchar()* após o scanf  do codigo para "limpar" o caractere que fica sobrando. Se não usar, o fgets lê o vazio.
+     // Use o fgets para inserção das cidades, pois algumas tem nomes compostos.
+
+     printf("Estado: ");
+     scanf(" %c", &estado2);  // O espaço antes de %c descarta o '\n' (Enter) deixado pelo scanf anterior no buffer.
+     printf("Código: ");
+     scanf("%s", codigo2);
+     getchar();
+     printf("Nome da Cidade: ");
+     fgets(cidade2, 30, stdin);
+     cidade2[strcspn(cidade2, "\n")]='\0';
+     printf("População: ");
+     scanf("%d", &populacao[1]);
+     printf("Área (em km²): ");
+     scanf("%f", &area[1]);
+     printf("PIB (em bilhões): ");
+     scanf("%f", &pib[1]);
+     printf("Número de Pontos Turísticos: ");
+     scanf("%d", &pontos_turisticos[1]);
+
+    
+     // **Cálculo do PIB per Capita, Densidade Populacional e do Super Poder**
+     // Feito a Conversão Explicita na variável população
+    
+     pib_per[1] = (pib[1] *  pow(10, 9)) / (float) populacao[1];
+     populcao_den[1] = (float) populacao[1] / area[1];
+     super_poder[1] = (float) populacao[1] + (float) pontos_turisticos[1] + pib[1] +
+                        area[1] + pib_per[1] + (1 / populcao_den[1]); 
+     
+     // ---LEMBRAR---
+     //*TODOS* os atributos adicionados terão de aparecer na apresentação da carta.
+     /* Exemplo:
+      printf("\nCarta 1\n");
+      printf("*ATRIBUTO ADICIONADO*: ESPECIFICADOR DE FORMATO\n", VARIÁVEL);*/
+
+     printf("\nCarta 2\n");
+     printf("Estado: %c\n", estado2);
+     printf("Código: %s\n", codigo2);
+     printf("Nome da Cidade: %s\n", cidade2);
+     printf("População: %d\n", populacao[1]);
+     printf("Área: %.2f km²\n", area[1]);
+     printf("PIB: %.2f bilhões de reais\n", pib[1]);
+     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos[1]);
+     printf("Densidade Populacional: %.2f hab/km²\n", populcao_den[1]);
+     printf("PIB per Capita: %.2f reais\n", pib_per[1]);
+     printf("Super Poder: %.2f\n\n", super_poder[1]);
+
+     //Placar Vencedor:
+
+     printf("Comparação de Cartas: \n\n");
+     printf("Atributo - População\n");
+     printf("Carta 1 (%s): %d\n", cidade1, populacao[0]);
+     printf("Carta 2 (%s): %d\n", cidade2, populacao[1]);
+        if(populacao[0] > populacao[1]){
+            printf(" Carta vencedora: Carta 1 (%s)\n\n", cidade1); 
+        }
+        else{
+            printf(" Carta vencedora: Carta 2 (%s)\n\n", cidade2); 
+        }
+    printf("Atributo - Área\n");
+     printf("Carta 1 (%s): %.2f\n", cidade1, area[0]);
+     printf("Carta 2 (%s): %.2f\n", cidade2, area[1]);
+        if(area[0] > area[1]){
+            printf(" Carta vencedora: Carta 1 (%s)\n\n", cidade1); 
+        }
+        else{
+            printf(" Carta vencedora: Carta 2 (%s)\n\n", cidade2); 
+        }
+     printf("Atributo - PIB\n");
+     printf("Carta 1 (%s): %.2f\n", cidade1, pib[0]);
+     printf("Carta 2 (%s): %.2f\n", cidade2, pib[1]);
+        if(pib[0] > pib[1]){
+            printf(" Carta vencedora: Carta 1 (%s)\n\n", cidade1); 
+        }
+        else{
+            printf(" Carta vencedora: Carta 2 (%s)\n\n", cidade2); 
+        }
+     printf("Atributo - Pontos Turísticos\n");
+     printf("Carta 1 (%s): %d\n", cidade1, pontos_turisticos[0]);
+     printf("Carta 2 (%s): %d\n", cidade2, pontos_turisticos[1]);
+        if(pontos_turisticos[0] > pontos_turisticos[1]){
+            printf(" Carta vencedora: Carta 1 (%s)\n\n", cidade1); 
+        }
+        else{
+            printf(" Carta vencedora: Carta 2 (%s)\n\n", cidade2); 
+        }
+     printf("Atributo - Densidade Populacional\n");
+     printf("Carta 1 (%s): %.2f\n", cidade1, populcao_den[0]);
+     printf("Carta 2 (%s): %.2f\n", cidade2, populcao_den[1]);
+        if(populcao_den[0] < populcao_den[1]){
+            printf(" Carta vencedora: Carta 1 (%s)\n\n", cidade1); 
+        }
+        else{
+            printf(" Carta vencedora: Carta 2 (%s)\n\n", cidade2); 
+        }
+     printf("Atributo - PIB per Capita\n");
+     printf("Carta 1 (%s): %.2f\n", cidade1, pib_per[0]);
+     printf("Carta 2 (%s): %.2f\n", cidade2, pib_per[1]);
+        if(pib_per[0] > pib_per[1]){
+            printf(" Carta vencedora: Carta 1 (%s)\n\n", cidade1); 
+        }
+        else{
+            printf(" Carta vencedora: Carta 2 (%s)\n\n", cidade2); 
+        }
+     printf("Atributo - Super Poder\n");
+     printf("Carta 1 (%s): %.2f\n", cidade1, super_poder[0]);
+     printf("Carta 2 (%s): %.2f\n", cidade2, super_poder[1]);
+        if(super_poder[0] > super_poder[1]){
+            printf(" Carta vencedora: Carta 1 (%s)\n\n", cidade1); 
+        }
+        else{
+            printf(" Carta vencedora: Carta 2 (%s)\n\n", cidade2); 
+        }
+
+
+
+     return 0;
+
+
+}
